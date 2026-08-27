5754b856708e26cdefb09a0767f64fb31dc66026
maki-engineer <ryohei-81@outlook.jp>
2026-08-27 13:26:52 +0900

    CIが動くか動作確認

--- backend/src/cmd/main.go
+++ backend/src/cmd/main.go
@@ -20,6 +20,7 @@ func main() {
 	repository := repository.NewMemberRepository(db)
 	useCase := usecase.NewMemberUseCase(repository)
 	handler := handler.NewMemberHandler(useCase)
+	// CIが動くか確認
 
 	r := route.SetupRoutes(handler)
 
